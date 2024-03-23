ccead9dd6193bf0fe37f1bddfc04d2c0903a5039
IAGrajeda <120614466+IAGrajeda@users.noreply.github.com>
2024-03-23 02:08:08 -0600

    feature ApiMinima

---
+++ ApiMinima/TodoItemDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiMinima
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public TodoItemDTO() { }
+        public TodoItemDTO(Todo todoItem) =>
+            (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
+    }
+}
